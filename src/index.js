9e7a3e3054c366a50ca9483fb99119c8f34812fa
Romil Rõbtšenkov <romilrobtsenkov@users.noreply.github.com>
2017-09-22 13:01:33 +0300

    added env tets

--- src/index.js
+++ src/index.js
@@ -3,29 +3,14 @@ const morgan = require('morgan')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 
-require('dotenv').config()
+const testEnvironment = process.env.NODE_ENV === 'test'
 
-mongoose.set('debug', true)
-mongoose.Promise = global.Promise
-mongoose.connect(process.env.MONGODB_URI, { useMongoClient: true })
-  .then(() => {
-    const listener = app.listen(process.env.APP_PORT || 3000, () =>
-      console.log('App started in ' +
-        process.env.NODE_ENV +
-        ' on port ' +
-        listener.address().port
-      )
-    )
-  })
-  .catch(() => {
-    console.error('Unable to connect to MongoDB')
-    process.exit(1)
-  })
+require('dotenv').config()
 
 const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(morgan('dev'))
+if (!testEnvironment) app.use(morgan('dev'))
 
 const words = require('./routes/words')
 app.use('/api/words', words)
@@ -43,5 +28,23 @@ app.use((err, req, res, next) => {
     message: process.env.NODE_ENV === 'development' ? err.message : ''
   })
 })
+
+if (!testEnvironment) mongoose.set('debug', true)
+mongoose.Promise = global.Promise
+mongoose.connect(process.env.MONGODB_URI, { useMongoClient: true })
+  .then(() => {
+    if (testEnvironment) return // do not start server for test env
+    const listener = app.listen(process.env.APP_PORT || 3000, () =>
+      console.log('App started in ' +
+        process.env.NODE_ENV +
+        ' on port ' +
+        listener.address().port
+      )
+    )
+  })
+  .catch(() => {
+    console.error('Unable to connect to MongoDB')
+    process.exit(1)
+  })
 
 module.exports = app
