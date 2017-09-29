551e969b92f2bb517b14aac287a8d7ca1856e9f5
Romil Rõbtšenkov <romilrobtsenkov@users.noreply.github.com>
2017-09-29 11:46:28 +0300

    added async middleware

--- src/controllers/words.js
+++ src/controllers/words.js
@@ -1,60 +1,35 @@
 const Word = require('../models/words')
 
 module.exports.getWords = async (req, res, next) => {
-  try {
-    const words = await Word.find({})
-    return res.json({ words })
-  } catch (err) {
-    console.error(err)
-    return res.status(400)
-  }
+  const words = await Word.find({})
+  return res.json({ words })
 }
 
 module.exports.getWord = async (req, res, next) => {
-  try {
     // console.log(req)
-    const { id } = req.params
-    const word = await Word.findById(id)
-    res.json({ word })
-  } catch (err) {
-    console.error(err)
-    return res.status(400)
-  }
+  const { id } = req.params
+  const word = await Word.findById(id)
+  res.json({ word })
 }
 
 module.exports.postWord = async (req, res) => {
-  try {
-    const { name } = req.body
-    const newWord = new Word({ name })
-    const word = await newWord.save()
+  const { name } = req.body
+  const newWord = new Word({ name })
+  const word = await newWord.save()
 
-    return res.status(201).send({ word })
-  } catch (err) {
-    console.error(err)
-    return res.status(400)
-  }
+  return res.status(201).send({ word })
 }
 
 module.exports.putWord = async (req, res) => {
-  try {
-    const { id } = req.params
-    const { name } = req.body
+  const { id } = req.params
+  const { name } = req.body
 
-    await Word.findOneAndUpdate({ _id: id }, { name })
-    return res.status(200).send()
-  } catch (err) {
-    console.error(err)
-    return res.status(400)
-  }
+  await Word.findOneAndUpdate({ _id: id }, { name })
+  return res.status(200).send()
 }
 
 module.exports.deleteWord = async (req, res) => {
-  try {
-    const { id } = req.params
-    await Word.findByIdAndUpdate(id, { deleted: new Date() })
-    return res.status(200).send()
-  } catch (err) {
-    console.error(err)
-    return res.status(400)
-  }
+  const { id } = req.params
+  await Word.findByIdAndUpdate(id, { deleted: new Date() })
+  return res.status(200).send()
 }
--- src/routes/words.js
+++ src/routes/words.js
@@ -3,10 +3,12 @@ const router = express.Router()
 const validate = require('../utils/validate')
 const words = require('../controllers/words')
 
-router.get('/', words.getWords)
-router.get('/:id', words.getWord)
-router.post('/', validate.word, words.postWord)
-router.put('/:id', validate.word, words.putWord)
-router.delete('/:id', words.deleteWord)
+const { asyncMiddleware } = require('../utils/common')
+
+router.get('/', asyncMiddleware(words.getWords))
+router.get('/:id', asyncMiddleware(words.getWord))
+router.post('/', validate.word, asyncMiddleware(words.postWord))
+router.put('/:id', validate.word, asyncMiddleware(words.putWord))
+router.delete('/:id', asyncMiddleware(words.deleteWord))
 
 module.exports = router
