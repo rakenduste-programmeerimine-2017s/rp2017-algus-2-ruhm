64fcb7e43c359f34bc185c188253f95cd9110d74
Romil Rõbtšenkov <romilrobtsenkov@users.noreply.github.com>
2017-09-29 10:56:26 +0300

    all routes now use async await

--- src/routes/words.js
+++ src/routes/words.js
@@ -30,58 +30,51 @@ router.post('/', [
   body('name').exists().withMessage('Name must exist'),
   body('name').isLength({ min: 2 }).withMessage('Name must be at least 2 characters long'),
   body('name').isAlphanumeric().withMessage('Name must contain only letters and numbers')
-], (req, res) => {
-  const errors = validationResult(req)
-  if (!errors.isEmpty()) return res.status(422).json({ errors: errors.mapped() })
+], async (req, res) => {
+  try {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) return res.status(422).json({ errors: errors.mapped() })
 
-  // console.log(req.body)
-  const { name } = req.body
+    const { name } = req.body
+    const newWord = new Word({ name })
+    const word = await newWord.save()
 
-  const newWord = new Word({ name })
-  // console.log(newWord)
-
-  newWord.save()
-    .then(word => {
-      return res.status(201).send({ word })
-    })
-    .catch(err => {
-      console.error(err)
-      return res.status(500).send({error: {msg: 'something went wrong here'}})
-    })
+    return res.status(201).send({ word })
+  } catch (err) {
+    console.error(err)
+    return res.status(400)
+  }
 })
 
 router.put('/:id', [
   body('name').exists().withMessage('Name must exist'),
   body('name').isLength({ min: 2 }).withMessage('Name must be at least 2 characters long'),
   body('name').isAlphanumeric().withMessage('Name must contain only letters and numbers')
-], (req, res) => {
-  const errors = validationResult(req)
-  if (!errors.isEmpty()) return res.status(422).json({ errors: errors.mapped() })
+], async (req, res) => {
+  try {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) return res.status(422).json({ errors: errors.mapped() })
 
-  const { id } = req.params
-  const { name } = req.body
+    const { id } = req.params
+    const { name } = req.body
 
-  Word.findOneAndUpdate({ _id: id }, { name })
-  .then(() => {
+    await Word.findOneAndUpdate({ _id: id }, { name })
     return res.status(200).send()
-  })
-  .catch(err => {
+  } catch (err) {
     console.error(err)
-    return res.status(500).send({error: {msg: 'something went wrong here'}})
-  })
+    return res.status(400)
+  }
 })
 
-router.delete('/:id', (req, res) => {
-  const { id } = req.params
-
-  Word.findByIdAndUpdate(id, { deleted: new Date() })
-  .then(() => {
+router.delete('/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+    await Word.findByIdAndUpdate(id, { deleted: new Date() })
     return res.status(200).send()
-  })
-  .catch(err => {
+  } catch (err) {
     console.error(err)
-    return res.status(500).send({error: {msg: 'something went wrong here'}})
-  })
+    return res.status(400)
+  }
 })
 
 module.exports = router
