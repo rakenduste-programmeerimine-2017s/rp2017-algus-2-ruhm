5f195b19fda4c50916c47692cd4c5292209f587e
Romil Rõbtšenkov <romilrobtsenkov@users.noreply.github.com>
2017-11-03 12:59:27 +0200

    jwt protect post words

--- src/routes/words.js
+++ src/routes/words.js
@@ -5,9 +5,11 @@ const words = require('../controllers/words')
 
 const { asyncMiddleware } = require('../utils/common')
 
+const { jwtCheck, jwtEnsure } = require('../utils/jwt')
+
 router.get('/', asyncMiddleware(words.getWords))
 router.get('/:id', asyncMiddleware(words.getWord))
-router.post('/', validate.word, asyncMiddleware(words.postWord))
+router.post('/', jwtCheck, jwtEnsure, validate.word, asyncMiddleware(words.postWord))
 router.put('/:id', validate.word, asyncMiddleware(words.putWord))
 router.delete('/:id', asyncMiddleware(words.deleteWord))
 
